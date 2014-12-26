744730bc9ca4d5235bec14b34eba9a1f242a601a
elizakuz <kizminaeliz@gmail.com>
2014-12-26 12:44:05 +0400

    stack

---
+++ stack.h
@@ -0,0 +1,48 @@
+#include "calculator.h"
+
+typedef struct stack{
+	long_num a;
+	struct stack * next;
+} stack;
+stack *hd = NULL, *p, *q, *r;
+void push(long_num n){
+	stack *p = (stack *) malloc(sizeof (stack));
+	p->a = n;
+	p->next = hd;
+	hd = p;	
+}
+long_num pop(){
+	long_num top;
+	top = hd->a;
+	p = hd;
+	hd = hd->next;	
+	free(p);
+	return top;
+}
+void stack_clean(){
+	while(hd != NULL){
+		p = hd;
+		hd = hd->next;
+		free(p);
+	}
+}	
+void print_stack(){
+	printf("[");
+	for(p = hd;p != NULL; p = p->next){
+		if(p->next != NULL){
+			if (p->a.sign == -1) {
+				printf("-");
+			}
+			print(p->a.digits.head);
+			printf(", ");
+		}else {
+			if (p->a.sign == -1) {
+				printf("-");
+			}
+			print(p->a.digits.head);
+		}
+	}
+	printf("]\n");
+}		
+
+	
